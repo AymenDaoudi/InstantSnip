57f70b01b8280613f689644c819d304969cd2e76
aymendaoudi <aymendaoudi@outlook.com>
2014-07-19 20:05:01 +0100

    Basic capture selection

--- InstantSnip/ViewModel/ScreenShotViewModel.cs
+++ InstantSnip/ViewModel/ScreenShotViewModel.cs
@@ -6,6 +6,7 @@ using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
 
 namespace InstantSnip.ViewModel
@@ -18,9 +19,7 @@ namespace InstantSnip.ViewModel
     /// </summary>
     public class ScreenShotViewModel : ViewModelBase
     {
-
-        private ImageSource _screenShotImageSource;
-
+        #region Properties
         public ImageSource ScreenShotImageSource
         {
             get
@@ -34,15 +33,87 @@ namespace InstantSnip.ViewModel
             }
         }
 
+
+        public Rect BackRect
+        {
+            get { return _backRect; }
+            set
+            {
+                _backRect = value;
+                RaisePropertyChanged("BackRect");
+            }
+        }
+
+        public Rect SelectionRect
+        {
+            get { return _selectionRect; }
+            set
+            {
+                _selectionRect = value;
+                RaisePropertyChanged("SelectionRect");
+            }
+        }
+
+        public double WindowWidth
+        {
+            get { return _windowWidth; }
+            set
+            {
+                _windowWidth = value;
+                RaisePropertyChanged("WindowWidth");
+            }
+        }
+
+        public double WindowHeight
+        {
+            get { return _windowHeight; }
+            set
+            {
+                _windowHeight = value;
+                RaisePropertyChanged("WindowHeight");
+            }
+        }
+
+        #endregion
+
+
+        #region RelayCommands
+
+        public RelayCommand WindowLoaded { get; set; }
+        #endregion
+
+
         public ScreenShotViewModel()
+        {
+            MessengerSubscriber();
+            InitRelayCommands();
+        }
+
+    
+
+        #region HelperMethods
+
+        private void MessengerSubscriber()
         {
             Messenger.Default.Register<Bitmap>(this, HandleScreenShotBitmap);
         }
+
 
         private void HandleScreenShotBitmap(Bitmap source)
         {
             ScreenShotImageSource = LoadBitmap(source);
         }
+
+
+        private void InitRelayCommands()
+        {
+            WindowLoaded = new RelayCommand(() =>
+            {
+                BackRect = new Rect(0, 0, WindowWidth, WindowHeight);
+                SelectionRect = new Rect(10, 10, 222, 222);
+            });
+        }
+
 
         [DllImport("gdi32")]
         static extern int DeleteObject(IntPtr obj);
@@ -64,5 +135,18 @@ namespace InstantSnip.ViewModel
 
             return bitmapSource;
         }
+    
+        #endregion
+        
+
+
+        #region Fields
+            private ImageSource _screenShotImageSource;
+            private Rect _backRect;
+            private Rect _selectionRect;
+            private double _windowWidth;
+            private double _windowHeight;
+
+        #endregion
     }
 }
